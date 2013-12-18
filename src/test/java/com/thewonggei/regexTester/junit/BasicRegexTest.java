f1133f6b17edcf0ebea8fcbd5f6dec903903fcb1
Nick Watts <nick.a.watts@gmail.com>
2013-12-17 20:08:37 -0500

    Added a very basic test of functionality.

--- src/test/java/com/thewonggei/regexTester/junit/BasicRegexTest.java
+++ src/test/java/com/thewonggei/regexTester/junit/BasicRegexTest.java
@@ -11,18 +11,26 @@ import com.thewonggei.regexTester.junit.RegexPojo;
 import com.thewonggei.regexTester.junit.RegexTestSuite;
 import com.thewonggei.regexTester.junit.RegexTestStrings;
 
+/**
+ * This test represents the most common usage of the
+ * Regex Tester library where a single regex is specified
+ * and a small list of test strings are given.
+ * 
+ * @author Nick Watts
+ * @since This style of library usage has been available since version 0.1.
+ */
 @RunWith(value=RegexTestSuite.class)
-@Regex(value="^xan.*")
-public class SampleParameterizedRegexTest {
+@Regex(value="^com.*")
+public class BasicRegexTest {
 	
 	@RegexTestStrings
 	public static List<RegexPojo> getTestParameters() {
 		return Arrays.asList(new RegexPojo[] {
-				new RegexPojo(true, "xander"),
-				new RegexPojo(false, "nick"),
-				new RegexPojo(true, "xantheman"),
-				new RegexPojo(false, "heisxan"),
-				new RegexPojo(false, "Xander")
+				new RegexPojo(true, "com"),
+				new RegexPojo(true, "com.thewonggei"),
+				new RegexPojo(true, "com.thewonggei.regexTester"),
+				new RegexPojo(false, ".com.thewonggei"),
+				new RegexPojo(false, "www.google.com")
 		});
 	}
 	
