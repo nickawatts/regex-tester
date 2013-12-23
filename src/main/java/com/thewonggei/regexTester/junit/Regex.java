3075502e2d05bc171ca7a4602bd07f8c3d5065a1
Nick Watts <nick.a.watts@gmail.com>
2013-12-22 21:38:01 -0500

    Filled in some documentation.

--- src/main/java/com/thewonggei/regexTester/junit/Regex.java
+++ src/main/java/com/thewonggei/regexTester/junit/Regex.java
@@ -5,8 +5,21 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to be used with a unit test class to provide the 
+ * regular expression string.
+ * 
+ * For example:
+ * <code>@Regex(value="^com.*")</code>
+ * 
+ * @author Nick Watts
+ * @since 0.1
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface Regex {
+	/**
+	 * @return The regular expression.
+	 */
 	public String value();
 }
--- src/main/java/com/thewonggei/regexTester/junit/RegexPojo.java
+++ src/main/java/com/thewonggei/regexTester/junit/RegexPojo.java
@@ -1,7 +1,21 @@
 package com.thewonggei.regexTester.junit;
 
+/**
+ * A property bag for the values needed to perform matching of a test
+ * string against a regular expression.
+ * 
+ * @author Nick Watts
+ * @since 0.1
+ */
 public class RegexPojo {
+	/**
+	 * True if the test string should result in a match against some
+	 * regular expression, false otherwise.
+	 */
 	public boolean shouldItMatch;
+	/**
+	 * The string against which to test a regular expression for a match.
+	 */
 	public String testString;
 	
 	public RegexPojo(boolean shouldItMatch, String testString) {
