199835b46022bfd5c6d7cbb10265415de8246b53
Nick Watts <nick.a.watts@gmail.com>
2014-04-12 22:46:53 -0400

    Devised a much simpler and more instructive regex test for basic usage.

--- src/test/java/com/thewonggei/regexTester/junit/BasicRegexTest.java
+++ src/test/java/com/thewonggei/regexTester/junit/BasicRegexTest.java
@@ -16,17 +16,19 @@ import com.thewonggei.regexTester.assertions.RegexAssertions;
  * @since This style of library usage has been available since version 0.2.
  */
 @RunWith(value=RegexTestSuite.class)
-@Regex(value="www\\.\\w+\\.")
+@Regex(value="[mM]ario")
 public class BasicRegexTest {
 	
 	@RegexAssertions
 	public static RegexAssertionSet getRegexAssertions() {
-		String input = "www.google.com";
+		String input = "Mario Andretti is a race car driver. There is a mario in the Super Mario Brothers line of video games.";
 		
 		return new RegexAssertionSetBuilder()
-			.addMatchesAtAssertion(1, "www.google.", input, true)
-			.addMatchesAtAssertion(1, "org.apache", input, false)
-			.addMatchesAtAssertion(1, "com.thewonggei", input, false)
+			.addMatchesAtAssertion(1, "Mario", input, true)
+			.addMatchesAtAssertion(1, "mario", input, false)
+			.addMatchesAtAssertion(2, "mario", input, true)
+			.addMatchesAtAssertion(3, "Mario", input, true)
+			.addMatchesAtAssertion(3, "mario", input, false)
 			.build();
 	}
 	
