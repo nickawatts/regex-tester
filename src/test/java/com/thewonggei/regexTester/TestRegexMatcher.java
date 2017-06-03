51ce1f6f2a06ae4235baf577ea381e63e05ffe4a
Nick Watts <nick.a.watts@gmail.com>
2017-06-02 22:04:30 -0400

    Added some comments and rearranged the tests in TestRegexMatcher to make them clearer.

--- src/test/java/com/thewonggei/regexTester/TestRegexMatcher.java
+++ src/test/java/com/thewonggei/regexTester/TestRegexMatcher.java
@@ -16,19 +16,29 @@ import static com.thewonggei.regexTester.hamcrest.RegexMatches.*;
  */
 public class TestRegexMatcher {
 
+    /*
+     * The first two tests show the most basic usage of the library.
+     * You either assert that a string matches a regex or that it doesn't.
+     */
+
 	@Test
 	public void test_a_string_that_matches_the_regex() {
 		assertThat("foo", doesMatchRegex("fo.+"));
 	}
 
-	@Test(expected=AssertionError.class)
-	public void test_a_string_that_does_not_match_the_regex__with_doesMatchRegex() {
-		assertThat("bar", doesMatchRegex("fo.+"));
-	}
-
 	@Test
 	public void test_a_string_that_does_not_match_the_regex() {
 		assertThat("bar", doesNotMatchRegex("fo.+"));
+	}
+
+    /*
+     * Another way to test is to assert that an AssertionError is thrown, which
+     * is a way to do negative testing.
+     */
+
+	@Test(expected=AssertionError.class)
+	public void test_a_string_that_does_not_match_the_regex__with_doesMatchRegex() {
+		assertThat("bar", doesMatchRegex("bar.+"));
 	}
 
 	@Test(expected=AssertionError.class)
